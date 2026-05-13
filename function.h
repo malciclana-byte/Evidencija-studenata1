c28a0e3f12c6c930b95e2c050f2c82cb7fdefb72
malciclana-byte <malciclana@gmail.com>
2026-05-13 10:52:26 +0200

    Create function.h

---
+++ function.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct student {
+	int id;
+	char* ime; //dinamicka alokacija imena
+	char* prezime;
+	float prosjek;
+	int godina;
+	char smjer[50];
+} STUDENT;
+
+void kreirajBazu(const char* const imeDat);
+void dodajNovogStudenta(const char* const imeDat);
+void ispisSvihIzDatoteke(const char* const imeDat);
+void provjeriAlokaciju(void* p);
+void ocistiBuffer();
+
+#endif
